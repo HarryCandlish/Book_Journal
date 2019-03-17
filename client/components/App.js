5e897b9fef95eedce0166c66c1a80c9248b7d5ef
HarryCandlish <hcnd90@gmail.com>
2019-03-18 09:58:40 +1300

    displaying home page

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+
+import Home from "./Home";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Book_Journal</h1>
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
       </div>
     );
   }

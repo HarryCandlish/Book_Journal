d14f9d09655068fbe844962f720bb01dea853c91
HarryCandlish <hcnd90@gmail.com>
2019-03-18 14:03:07 +1300

    touched up navbar

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -4,26 +4,28 @@ class Nav extends Component {
   render() {
     return (
       <div>
-        <header>
-          <div className="container">
-            <nav>
-              <ul>
-                <li>
-                  <a href="/#/Dash">Personal-Dash</a>
-                </li>
-                <li>
-                  <a href="/#/Books">Books</a>
-                </li>
-                <li>
-                  <a href="/#/Reviews">Reviews</a>
-                </li>
-                <li>
-                  <a href="/#/About">About</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </header>
+        <div className="navgrid">
+          <header>
+            <div className="container">
+              <nav>
+                <ul>
+                  <li>
+                    <a href="/#/Dash">Personal-Dash</a>
+                  </li>
+                  <li>
+                    <a href="/#/Books">Books</a>
+                  </li>
+                  <li>
+                    <a href="/#/Reviews">Reviews</a>
+                  </li>
+                  <li>
+                    <a href="/#/About">About</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </header>
+        </div>
       </div>
     );
   }
